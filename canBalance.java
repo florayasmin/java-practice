b7c828dabceb27ad53722130aa2ed03165b568ad
Flora <55338271+florayasmin@users.noreply.github.com>
2025-12-06 12:12:05 -0800

    recursive backtracking practice with a balancing problem
    fixed solution

--- canBalance.java
+++ canBalance.java
@@ -1,10 +1,6 @@
 // recursive backtracking
 // supposed to do without a loop
 public boolean canBalance(int target, List<Integer> weights) {
-    return canBalance(target, weights, 0, 0);
-}
-
-private boolean canBalance(int target, List<Integer> weights, int index, int currentSum) {
     // check if it's even possible
     int sumOfWeights = 0;
     for (int w : weights) {
@@ -13,11 +9,13 @@ private boolean canBalance(int target, List<Integer> weights, int index, int cur
     if (sumOfWeights < target) {
         return false;
     }
+    return canBalance(target, weights, 0, 0);
+}
 
-    // choose-explore-unchoose
+private boolean canBalance(int target, List<Integer> weights, int index, int currentSum) {
     if (currentSum == target) {
         return true;
-    } else if (currentSum < target && index < weights.size() - 1) {
+    } else if (currentSum < target && index < weights.size() - 1) { // choose-explore-unchoose
         currentSum += weights.get(index);
         canBalance(target, weights, index++, currentSum);
         currentSum -= weights.get(index);
